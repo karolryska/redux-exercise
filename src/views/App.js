5dff3490aca68587f9d73813a1529335823c9dc2
Karol Ryska <karolryska@gmail.com>
2021-12-06 21:38:36 +0100

    feat: add routing

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Users from './Users';
+
 const App = () => {
-    return <div></div>;
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<Users />} />
+        </Routes>
+    );
 };
 
 export default App;
